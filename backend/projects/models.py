ba69436d696de159a9a551870df30e07e5a9bd4b
Peyton Riley <psriley@ksu.edu>
2022-11-30 12:15:13 -0600

    Make initial model migration

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length=200)
+    nces_id = models.PositiveBigIntegerField()
+    created_on = models.DateTimeField(default=timezone.now)
+    updated_on = models.DateTimeField(default=timezone.now)
+    updated_by = models.ForeignKey('User', on_delete=models.CASCADE, default=None, blank=True, null=True)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    user_type = models.CharField(max_length=50)
+    school_id = models.ForeignKey(School, on_delete=models.CASCADE)
+    active = models.BooleanField(default=False)
+    password_hash = models.CharField(verbose_name='password', max_length=250)
+    salt = models.CharField(max_length=250)
+    created_on = models.DateTimeField(default=timezone.now)
+    updated_on = models.DateTimeField(default=timezone.now)
+    updated_by = models.ForeignKey('User', on_delete=models.CASCADE, default=None, blank=True, null=True)
+
+
+class Class(models.Model):
+    name = models.CharField(max_length=200)
+    passphrase_hash = models.CharField(max_length=250)
+    salt = models.CharField(max_length=250)
+    active = models.BooleanField()
+    created_on = models.DateTimeField(default=timezone.now)
+    updated_on = models.DateTimeField(default=timezone.now)
+    updated_by = models.ForeignKey('User', related_name='class_user', on_delete=models.CASCADE)
+
+
+class ClassUsers(models.Model):
+    class_id = models.ForeignKey(Class, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Project(models.Model):
+    user = models.ForeignKey(User, related_name="project_user", on_delete=models.CASCADE)
+    teacher = models.ForeignKey(User, related_name="teacher", on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=50)
+    created_on = models.DateTimeField(default=timezone.now)
+    updated_on = models.DateTimeField(default=timezone.now)
+    updated_by = models.ForeignKey('User', on_delete=models.CASCADE)
