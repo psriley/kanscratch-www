0a9c288b7eae612a65654cb34d135494fb7bbad4
psriley <psriley@ksu.edu>
2023-03-16 00:54:20 -0500

    Load class list items from database api call

--- frontend/src/components/item_box.js
+++ frontend/src/components/item_box.js
@@ -1,85 +1,45 @@
-import React, {useState} from "react";
+import React from "react";
+import axios from "axios";
 
 /**
- * Functional component that contains the boxes containing lists 
+ * Functional component that contains the boxes containing lists
  * of items (projects, classrooms, etc.)
  * @function
- * @param text = Whether the item box should contain classrooms or projects.
+ * @property text = Whether the item box should contain classrooms or projects.
  */
-function ItemBox({text}) {
-    let state = null;
+export default class ItemList extends React.Component {
+  state = {
+    classes: [],
+    projects: []
+  }
 
-    if (text === "Classroom")
-    {
-        state = {
-            type: 'classrooms',
-            listitems: [
-                {
-                    id: 0,
-                    text: 'Classroom 1',
-                    modifier: 'ItemButton blue'
-                },
-                {
-                    id: 1,
-                    text: 'Classroom 2',
-                    modifier: 'ItemButton blue'
-                },
-                {
-                    id: 2,
-                    text: 'Classroom 3',
-                    modifier: 'ItemButton green'
-                },
-                {
-                    id: 3,
-                    text: 'Classroom 4',
-                    modifier: 'ItemButton green'
-                }
-            ]
-        };
-    } else {
-        state = {
-            type: 'projects',
-            listitems: [
-                {
-                    id: 0,
-                    text: 'Project 1',
-                    modifier: 'ItemButton blue'
-                },
-                {
-                    id: 1,
-                    text: 'Project 2',
-                    modifier: 'ItemButton blue'
-                },
-                {
-                    id: 2,
-                    text: 'Project 3',
-                    modifier: 'ItemButton green'
-                },
-                {
-                    id: 3,
-                    text: 'Project 4',
-                    modifier: 'ItemButton green'
-                }
-            ]
-        };
-    }
+  componentDidMount() {
+    axios.get('http://localhost:8000/api/classes')
+      .then(res => {
+        if (this.props.text === 'Classroom') {
+            const classes = res.data;
+            this.setState({classes});
+        }
+      })
+  }
 
-
+  render() {
     return (
         <div className='ClassroomBox'>
-            <div className={state.type}>
+            <div className="classrooms">
                 <ul>
-                    {state.listitems.map(listitem => (
-                        <a className={listitem.modifier} key={listitem.id} href={`${state.type}/${listitem.id}/`}>
-                            <li title={listitem.text}>
-                                { listitem.text }
+                    {
+                        this.state.classes.map((c, i) => (
+                        <a className="ItemButton green" key={i} href={`classrooms/${i}/`}>
+                            <li title={c.name}>
+                                { c.name }
                             </li>
                         </a>
-                    ))}
+                        ))
+                    }
                 </ul>
             </div>
         </div>
-    );
+    )
+  }
 }
-
-export default ItemBox
